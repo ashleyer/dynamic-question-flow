7b8cfd626a501bacbdd2d8d0bc53fc8ab4fee0d2
ashleyromano <ashleye.romano@gmail.com>
2024-07-24 16:55:54 -0400

    fixing git

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <router-view></router-view>
+    <div>{{ message }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello Vue!'
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* Your styles here */
+</style>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+const Home = {
+    template: `<div><h2>Test Vue App</h2></div>`
+  };
+  
+  const routes = [
+    { path: '/', component: Home }
+  ];
+  
+  const router = new VueRouter({
+    routes
+  });
+  
+  new Vue({
+    el: '#app',
+    router,
+    data: {
+      message: 'Hello Vue!'
+    }
+  });
